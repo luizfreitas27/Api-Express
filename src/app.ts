4baffa3be7125ca0a238c7a43f6f90e0ce7a8534
luiz freias <luizfreitas2712@hotmail.com>
2024-03-12 11:04:12 -0400

    second

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { json } from "express";
+import helmet from "helmet";
 import { booksRouter } from "./routes/books.routes";
 import { HandleErrors } from "./middlewares/handle.middlewares";
 import "express-async-errors";
 
 export const app = express();
+
+app.use(helmet());
 
 app.use(json());
 
